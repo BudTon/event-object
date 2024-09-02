0220e3fc21a9a01e9d59dd89c7c483c4a96eb292
Bud_Ton <d2023ujle@gmail.com>
2024-09-02 23:22:19 +0300

    HW commit

--- tabs/task.js
+++ tabs/task.js
@@ -13,12 +13,24 @@ let indexRemove = 0;
 //         listTabs[indexRemove].classList.add('tab_active')
 //         listTabContents[index].classList.add('tab__content_active')
 //     }
-
 // }
 
 // Вариант 2
-listTabs.forEach((tab, index) => {
+// listTabs.forEach((tab, index) => {
+//     tab.onclick = () => {
+//         listTabs[indexRemove].classList.remove('tab_active')
+//         listTabContents[indexRemove].classList.remove('tab__content_active')
+//         indexRemove = index;
+//         listTabs[index].classList.add('tab_active')
+//         listTabContents[index].classList.add('tab__content_active')
+//     }
+// });
+
+
+// Вариант 3
+listTabs.forEach((tab) => {
     tab.onclick = () => {
+        const index = listTabs.indexOf(tab);
         listTabs[indexRemove].classList.remove('tab_active')
         listTabContents[indexRemove].classList.remove('tab__content_active')
         indexRemove = index;
